8a5308b5e0b7336e8d1dda0df06950c6b7afd7d3
Makmaer <instrojgrad@gmail.com>
2022-07-12 23:09:30 +0500

    Задачи из лекции 4. Факториал

---
+++ Zadacha3_Faktorial/Program.cs
@@ -0,0 +1,10 @@
+﻿double Faktorial (int n)
+{
+    if(n == 1) return 1;
+    else return n*Faktorial (n - 1) ;
+
+}
+for (int i = 1; i < 40; i++)
+{
+   Console.WriteLine( i + "! " + Faktorial(i)); 
+}
