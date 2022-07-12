9f93ee7457bceb61e99a87ef7fffc213d8bce94a
Makmaer <instrojgrad@gmail.com>
2022-07-12 23:20:53 +0500

    Задачи из лекции 4. Фибоначи

---
+++ Zadacha4_Fibonachi/Program.cs
@@ -0,0 +1,10 @@
+﻿int Fib (int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fib (n-1) + Fib (n-2);
+}
+
+for (int i = 1; i < 45; i++)
+{
+   Console.WriteLine($"f({i}) = {Fib(i)}");
+}
